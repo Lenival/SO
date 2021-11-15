b9b5c204320558d0d8dee3ee69ecd0ecf76e5c75
Lenival <lenival@ufrn.edu.br>
2021-11-15 14:17:22 -0300

    New matrix builder
    New matrix builder schema, file name pattern and input options

--- T01/auxiliar.c
+++ T01/auxiliar.c
@@ -3,25 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void rand_matrix_builder(time_t seconds,float **matrix,int L, int C){
-	srand((unsigned int)seconds);	// Random seed
-	printf("Building matrix...\n");
-	for (int l = 0; l < L; l++)
-		for (int c = 0; c < C; c++)
-			matrix[l][c] = 10*((float)(rand()-(RAND_MAX>>1))/((float)(RAND_MAX)));
-}
-
-void matrix_printer(float **matrix,int L, int C){	
-	for (int l = 0; l < L; l++){
-		printf("\n");
-		for (int c = 0; c < C; c++)
-			printf("a(%d,%d)=%.2f\t",l,c,matrix[l][c]);
-	}
-	printf("\n\n");
-}
-
-
-
 int main (int argc, char *argv[]) {
 	// Initializations
 	time_t seconds;
@@ -32,52 +13,34 @@ int main (int argc, char *argv[]) {
 		
 	// Getting parameters
 	int dims[4];
-	for(int i=1;i<argc;i++){
+	for(int i=1;i<argc;i++)
 		dims[i-1] = atoi(argv[i]);
-		printf("%d string: %s inteiro: %d\n", i,argv[i],dims[i-1]);
-	}
-	
-	// Building matrices
-	float **m1=(float**)malloc((dims[0])*sizeof(float*));
-	for(int l = 0 ; l < dims[0] ; l++)
-		m1[l]=(float*)malloc((dims[1])*sizeof(float));
-	rand_matrix_builder(seconds,m1,dims[0],dims[1]);
-	//matrix_printer(m1,dims[0],dims[1]);
-	printf("a(%d,%d)=%.2f\n",dims[0],dims[1],m1[dims[0]-1][dims[1]-1]);
-	
-	float **m2=(float**)malloc((dims[2])*sizeof(float*));
-	for(int l = 0 ; l < dims[2] ; l++)
-		m2[l]=(float*)malloc((dims[3])*sizeof(float));
-	rand_matrix_builder(seconds+1,m2,dims[2],dims[3]);
-	//matrix_printer(m2,dims[2],dims[3]);
-	printf("a(%d,%d)=%.2f\n",dims[2],dims[3],m2[dims[2]-1][dims[3]-1]);
-			
-	
 	
 	int length = snprintf( NULL, 0, "%ld", seconds );
 	char *now = malloc(length + 1);
 	snprintf( now, length + 1, "%ld", seconds );
 	
-	srand((unsigned int)seconds);	// Random seed
 	
-	
-	printf("Seconds since January 1, 1970 = %ld\n", seconds);
-	printf("Número aleatório %f\n", 10*((float)(rand()-(RAND_MAX>>1))/((float)(RAND_MAX))));
-	printf("Número aleatório %f\n", 10*((float)(rand()-(RAND_MAX>>1))/((float)(RAND_MAX))));
-	printf("string: %s | long int: %ld\n",now, seconds);
-	
-	
-	
-	// Saving  matrices
+	// Matrices file names
 	char m1_name[100] = "";
-	strcat(m1_name,now);
-	strcat(m1_name,"_m1");
-	strcat(m1_name,".dat");
+	sprintf(m1_name,"%dx%d_",dims[0],dims[1]);
 	char m2_name[100] = "";
-	strcat(m2_name,now);
-	strcat(m2_name,"_m2");
-	strcat(m2_name,".dat");
+	sprintf(m2_name,"%dx%d_",dims[2],dims[3]);
+	if(argc < 7){
+		strcat(m1_name,now);
+		strcat(m2_name,now);
+		strcat(m1_name,"_m1");
+		strcat(m1_name,".dat");
+		strcat(m2_name,"_m2");
+		strcat(m2_name,".dat");
+	}else{
+		strcat(m1_name,argv[5]);
+		strcat(m2_name,argv[6]);
+	}
 	
+	// Matrices creation and store
+	
+	srand((unsigned int)seconds);	// Random seed
 	FILE * fp1;
 	fp1 = fopen (m1_name, "w+");
 	fprintf(fp1,"%d %d\n",dims[0],dims[1]);
@@ -90,21 +53,10 @@ int main (int argc, char *argv[]) {
 	for (int l = 0; l < dims[2]; l++)
 		for (int c = 0; c < dims[3]; c++)
 			fprintf(fp2,"c%d%d %f\n",l,c,10*((float)(rand()-(RAND_MAX>>1))/((float)(RAND_MAX))));
-	
-	
-	
+		
 	fclose(fp1);
 	fclose(fp2);
-	//getchar();
-	free(now);
-	for(int l = 0 ; l < dims[0] ; l++)
-		free(m1[l]);
-	free(m1);
-	for(int l = 0 ; l < dims[2] ; l++)
-		free(m2[l]);
-	free(m2);
-	//getchar();
-	
+	free(now);	
 	
 	return(0);
 }
