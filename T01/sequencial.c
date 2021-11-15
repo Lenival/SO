b21d3ccf6a98356b3fe7067da3ea8982a40aec73
Lenival <lenival@ufrn.edu.br>
2021-11-15 14:19:13 -0300

    Begin sequencial.c
    Start of matrices product sequential routine

---
+++ T01/sequencial.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+void rand_matrix_builder(time_t seconds,float **matrix,int L, int C){
+	srand((unsigned int)seconds);	// Random seed
+	printf("Building matrix...\n");
+	for (int l = 0; l < L; l++)
+		for (int c = 0; c < C; c++)
+			matrix[l][c] = 10*((float)(rand()-(RAND_MAX>>1))/((float)(RAND_MAX)));
+}
+
+void matrix_printer(float **matrix,int L, int C){	
+	for (int l = 0; l < L; l++){
+		printf("\n");
+		for (int c = 0; c < C; c++)
+			printf("a(%d,%d)=%.2f\t",l,c,matrix[l][c]);
+	}
+	printf("\n\n");
+}
+
+
+
+int main (int argc, char *argv[]) {
+	// Initializations
+	clock_t tic, toc;
+	tic = clock();
+	
+	time_t s_start, s_end;
+	time(&s_start);
+	
+	char dummy_s[100];
+	float dummy_f;
+	
+	int L1,C1,L2,C2;
+		if(argc < 3)		// Are there 2 matrices names?
+			return -1;
+		
+	
+	// Reading matrices
+	FILE * fp1;
+	fp1 = fopen (argv[1], "r");
+	fscanf(fp1,"%d %d",&L1,&C1);
+	
+	float **m1=(float**)malloc(L1*sizeof(float*));
+	for(int l = 0 ; l < L1 ; l++){
+		m1[l]=(float*)malloc(C1*sizeof(float));
+		for (int c=0; c < C1; c++)
+			fscanf(fp1,"%s %f", dummy_s, &(m1[l][c]));
+	}
+	//matrix_printer(m1,L1, C1);
+		
+	FILE * fp2;
+	fp2 = fopen (argv[2], "r");
+	fscanf(fp2,"%d %d",&L2,&C2);
+	
+	float **m2=(float**)malloc(L2*sizeof(float*));
+	for(int l = 0 ; l < L2 ; l++){
+		m2[l]=(float*)malloc(C2*sizeof(float));
+		for (int c=0; c < C2; c++)
+			fscanf(fp2,"%s %f", dummy_s, &(m2[l][c]));
+	}
+	//matrix_printer(m2,L2,C2);
+	
+	float **result=(float**)malloc(L1*sizeof(float*));
+	for(int l = 0 ; l < L1 ; l++){
+		result[l]=(float*)calloc(C2,sizeof(float));
+		for (int c=0; c < C2; c++)
+			for (int rc=0; rc < C1; rc++)
+				result[l][c]+=m1[l][rc]*m2[rc][c];
+	}
+	matrix_printer(result,L1,C2);
+	
+	FILE * fp3;
+	char result_name[100];
+	char str_temp1[100];
+	char str_temp2[100];
+	sprintf(result_name,"%s_%s_result.dat",strncpy(str_temp1, argv[1], strlen(argv[1])-4),strncpy(str_temp2, argv[2], strlen(argv[2])-4));
+	//printf("\n\n%s\n\n", result_name);
+	fp3 = fopen (result_name, "w+");
+	for (int l = 0; l < L1; l++)
+		for (int c = 0; c < C2; c++)
+			fprintf(fp3,"c%d%d %f\n",l,c,result[l][c]);
+	
+	
+	
+	for(int l = 0 ; l < L1 ; l++)
+		free(m1[l]);
+	free(m1);
+	for(int l = 0 ; l < L2 ; l++)
+		free(m2[l]);
+	free(m2);
+	for(int l = 0 ; l < L1 ; l++)
+		free(result[l]);
+	free(result);
+	fclose(fp1);
+	fclose(fp2);
+	toc = clock();
+	fclose(fp3);
+	printf("\nThe process takes %f clocks cicles\n", ((double)(toc-tic)))/CLOCKS_PER_SEC;
+	time(&s_end);
+	printf("\nThe process takes %li seconds\n", s_end-s_start);
+	return(0);
+}
