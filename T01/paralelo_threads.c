32f4d556e2faeeb3608a36382c0d93f7f72bbf4e
Lenival <lenival@ufrn.edu.br>
2021-11-21 08:23:30 -0300

    Update paralelo_threads.c
    "paralelo threads.c" according to the specification

--- T01/paralelo_threads.c
+++ T01/paralelo_threads.c
@@ -5,9 +5,10 @@
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 
-#define TID (int)(size_t)tid
+#define TID_CONV (int)(size_t)tid
 	
 //pthread_t threads[NTHREAD];
 pthread_t* threads;
@@ -23,6 +24,12 @@ int* l_ini;
 int* l_end; 
 int* c_ini; 
 int* c_end; 
+FILE** fpv;
+char** threads_file_names;
+char* output_dir_name;
+unsigned int ifnames_len[2];
+char* input_files_names[2];
+time_t *thread_tic, *thread_toc;
 
 void matrix_printer(float **matrix,int L, int C){	
 	for (int l = 0; l < L; l++){
@@ -34,7 +41,15 @@ void matrix_printer(float **matrix,int L, int C){
 }
 
 void* matrix_paralel_product(void* tid){
-	//sleep(TID);
+	int TID = TID_CONV;					// void* tid to int
+	
+	// Output file creation
+	threads_file_names[TID] = (char*)malloc((2*(ifnames_len[0]+ifnames_len[1])+50)*sizeof(char));
+	sprintf(threads_file_names[TID],"thr/%s_%s_%d/%s_%s_thr_%d_result.dat",input_files_names[0],input_files_names[1],P,input_files_names[0],input_files_names[1],TID);
+	//printf("Thread %d file name\n\t%s\n\n",TID,threads_file_names[TID]);
+	fpv[TID] = fopen(threads_file_names[TID], "w+");
+	fprintf(fpv[TID],"%d %d\n",L1,C2);
+	
 	index_ini[TID] = P*(TID);
 	l_ini[TID] = index_ini[TID]/C2;
 	l_end[TID] = ((index_ini[TID]+P)/C2);
@@ -44,7 +59,8 @@ void* matrix_paralel_product(void* tid){
 		l_end[TID]=L1-1;
 		c_end[TID]=C2;
 	}
-	printf("TID=%d index_ini[TID]:%d>>l_ini[TID]: %d\tl_end[TID]: %d\tc_ini[TID]: %d\tc_end[TID]: %d\n",TID,index_ini[TID],l_ini[TID], l_end[TID],c_ini[TID],c_end[TID]);
+	//printf("TID=%d index_ini[TID]:%d>>l_ini[TID]: %d\tl_end[TID]: %d\tc_ini[TID]: %d\tc_end[TID]: %d\n",TID,index_ini[TID],l_ini[TID], l_end[TID],c_ini[TID],c_end[TID]);
+	
 	// Matrix product
 	float temp;
 	for(int l = l_ini[TID] ; l < l_end[TID] ; l++){
@@ -52,7 +68,8 @@ void* matrix_paralel_product(void* tid){
 			temp = 0;
 			for (int rc=0; rc < C1; rc++)
 				temp+=m1[l][rc]*m2[rc][c];
-			printf("c%d%d %f\n",l,c,temp);
+			//printf("c%d%d %f\n",l,c,temp);
+			fprintf(fpv[TID],"c%d%d %f\n",l,c,temp);
 		}
 		c_ini[TID] = 0;
 	}
@@ -60,8 +77,12 @@ void* matrix_paralel_product(void* tid){
 			temp = 0;
 			for (int rc=0; rc < C1; rc++)
 				temp+=m1[l_end[TID]][rc]*m2[rc][c];
-			printf("c%d%d %f\n",l_end[TID],c,temp);
+			//printf("c%d%d %f\n",l_end[TID],c,temp);
+			fprintf(fpv[TID],"c%d%d %f\n",l_end[TID],c,temp);
 	}
+	time(&thread_toc[TID]);
+	fprintf(fpv[TID],"%li ",thread_toc[TID]-thread_tic[TID]);
+	fclose(fpv[TID]);
 	pthread_exit(NULL);
 }
 
@@ -79,9 +100,31 @@ int main (int argc, char *argv[]) {
 	
 	if(argc < 4)		// Are there 2 matrices names?
 		return -1;
-		
 	
-	// Reading matrices
+	// Getting P
+	P = atoi(argv[3]);
+	
+	// Getting files info
+	ifnames_len[0] = strlen(argv[1]);
+	input_files_names[0]= (char*)malloc((ifnames_len[0]-4)*sizeof(char));
+	strncpy(input_files_names[0], argv[1], ifnames_len[0]-4);
+	
+	ifnames_len[1] = strlen(argv[2]);
+	input_files_names[1]= (char*)malloc((ifnames_len[1]-4)*sizeof(char));
+	strncpy(input_files_names[1], argv[2], ifnames_len[1]-4);
+	
+	output_dir_name = (char*)malloc((ifnames_len[0]+ifnames_len[1]+10)*sizeof(char));
+	sprintf(output_dir_name,"thr/%s_%s_%d",input_files_names[0],input_files_names[1],P);
+	printf("%s\n",output_dir_name);
+	struct stat st = {0};
+
+	if (stat(output_dir_name, &st) == -1) {
+		mkdir(output_dir_name, 0666);
+	}else{
+		printf("Já existe o diretório\n");
+	}
+	
+	// Reading matrix 1
 	FILE * fp1;
 	fp1 = fopen (argv[1], "r");
 	fscanf(fp1,"%d %d",&L1,&C1);
@@ -92,9 +135,12 @@ int main (int argc, char *argv[]) {
 		for (int c=0; c < C1; c++)
 			fscanf(fp1,"%s %f", dummy_s, &(m1[l][c]));
 	}
+	fclose(fp1);   // Closing matrix files
 	//matrix_printer(m1,L1, C1);
-		
+	
+	// Reading matrix 1
 	FILE * fp2;
+	ifnames_len[1] = strlen(argv[2]);
 	fp2 = fopen (argv[2], "r");
 	fscanf(fp2,"%d %d",&L2,&C2);
 	
@@ -104,36 +150,40 @@ int main (int argc, char *argv[]) {
 		for (int c=0; c < C2; c++)
 			fscanf(fp2,"%s %f", dummy_s, &(m2[l][c]));
 	}
+	fclose(fp2);	// Closing matrix files
+	
 	//matrix_printer(m2,L2,C2);
 	
-	float **result=(float**)malloc(L1*sizeof(float*));
+/* 	float **result=(float**)malloc(L1*sizeof(float*));
 	for(int l = 0 ; l < L1 ; l++){
 		result[l]=(float*)calloc(C2,sizeof(float));
 		for (int c=0; c < C2; c++)
 			for (int rc=0; rc < C1; rc++)
 				result[l][c]+=m1[l][rc]*m2[rc][c];
-	}
+	} */
 	//matrix_printer(result,L1,C2);
 	
-	// Closing matrix files
-	fclose(fp1);
-	fclose(fp2);	
 	
-	// Getting P
-	P = atoi(argv[3]);
-	printf("Matrix dimensions>> L1: %d\tC1: %d\tL2: %d\tC2: %d\n",L1, C1, L2, C2);
-	//int nthreads = ceil((float)(((float)L1*C2)/P));
+	//printf("Matrix dimensions>> L1: %d\tC1: %d\tL2: %d\tC2: %d\n",L1, C1, L2, C2);
+	
+	// Preparing threads and auxiliary variables
 	nthreads = ceil((((float)L1*C2)/P));
 	index_ini = (int*)malloc(nthreads*sizeof(int));
 	l_ini = (int*)malloc(nthreads*sizeof(int)); 
 	l_end = (int*)malloc(nthreads*sizeof(int)); 
 	c_ini = (int*)malloc(nthreads*sizeof(int)); 
 	c_end = (int*)malloc(nthreads*sizeof(int)); 
-	printf("nthreads: %d\tL1: %d\tC2: %d\tP: %d\n",nthreads,L1,C2,P);
+	//printf("nthreads: %d\tL1: %d\tC2: %d\tP: %d\n",nthreads,L1,C2,P);
 	threads = (pthread_t*)malloc(nthreads*sizeof(pthread_t));
+	fpv = (FILE**)malloc(nthreads*sizeof(FILE*));
+	threads_file_names = (char**)malloc(nthreads*sizeof(char*));
+	thread_tic = (time_t*)malloc(nthreads*sizeof(time_t));
+	thread_toc = (time_t*)malloc(nthreads*sizeof(time_t));
+
 	
 	for (int i = 0; i < nthreads; i++){
-		printf("Creating threads\n");
+		//printf("Creating threads\n");
+		time(&thread_tic[i]); 
 		status = pthread_create(&threads[i], NULL, matrix_paralel_product, (void *)(size_t) i);
 		if (status){
 			printf("Error in thread %d creation\n",i);
@@ -142,9 +192,9 @@ int main (int argc, char *argv[]) {
 	}
 	
 	for (int i = 0; i < nthreads; i++){
-		printf("Waiting for thread %d\n",i);
+		//printf("Waiting for thread %d\n",i);
 		status = pthread_join(threads[i], &thread_return);
-		printf("The thread %d is over\n",i);
+		//printf("The thread %d is over\n",i);
 		if (status){
 			printf("Error in thread %d end\n",i);
 			return -1;
@@ -154,21 +204,21 @@ int main (int argc, char *argv[]) {
 	
 	
 	// Saving 
-	FILE * fp3;
+/* 	FILE * fp3;
 	char result_name[100]="";
 	char str_temp1[100]="";
 	char str_temp2[100]="";
-	sprintf(result_name,"%s_%s_result.dat",strncpy(str_temp1, argv[1], strlen(argv[1])-4),strncpy(str_temp2, argv[2], strlen(argv[2])-4));
+	sprintf(result_name,"thr/%s_%s_result.dat",strncpy(str_temp1, argv[1], strlen(argv[1])-4),strncpy(str_temp2, argv[2], strlen(argv[2])-4));
 	fp3 = fopen (result_name, "w+");
-	fprintf(fp3,"%d %d\n",L1,C2);
-	printf("result_name: %s \n",result_name);
+	fprintf(fp3,"%d %d\n",L1,C2); */
+	//printf("result_name: %s \n",result_name);
 	//puts("It's here\n");
 	
-	
+/* 	
 	for (int l = 0; l < L1; l++)
 		for (int c = 0; c < C2; c++)
 			fprintf(fp3,"c%d%d %f\n",l,c,result[l][c]);
-	
+	 */
 	
 	// Freeing memory
 	for(int l = 0 ; l < L1 ; l++)
@@ -186,13 +236,17 @@ int main (int argc, char *argv[]) {
 	free(l_end); 
 	free(c_ini); 
 	free(c_end); 
-	
+	free(input_files_names[0]);
+	free(input_files_names[1]);
+	free(output_dir_name);
 	// Final time measurment
 	toc = clock();
-	printf("\nThe process takes %f clocks cicles\n", ((double)(toc-tic)))/CLOCKS_PER_SEC;
+	//printf("\nThe process takes %f clocks cicles\n", ((double)(toc-tic)))/CLOCKS_PER_SEC;
 	time(&s_end);
-	fprintf(fp3,"%li\n",s_end-s_start);
-	fclose(fp3);
-	printf("\nThe process takes %li seconds\n", s_end-s_start);
+/* 	fprintf(fp3,"%li\n",s_end-s_start);
+	fclose(fp3); */
+	//fclose(fpv);
+	//fclose(threads_file_names);
+	//printf("\nThe process takes %li seconds\n", s_end-s_start);
 	return(0);
 }
